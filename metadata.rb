aeaca84c8a90345504bf6b8d99ab2c111f803885
Allan Espinosa <allan.espinosa@outlook.com>
2016-02-22 12:00:47 -0600

    Write description and initial pipeline

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'dockerhp'
+description 'Base infrastructure described in the book, Docker High Performance'
+maintainer 'Allan Espinosa'
+maintainer_email 'allan.espinosa@outlook.com'
+
+version '0.1.0'
+
+depends 'apt'
+depends 'docker'
